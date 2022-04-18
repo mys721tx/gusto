544730ff3440241602424874e05a8aa52db4ecac
Yishen Miao <mys721tx@gmail.com>
2022-04-18 13:07:17 -0700

    Refactoring GRAS
    Refactors GRAS with argparser.

--- gras.R
+++ gras.R
@@ -4,24 +4,33 @@ if (!require("pacman")) {
     install.packages("pacman")
 }
 
-pacman::p_load("tidyverse", "optparse")
+pacman::p_load("tidyverse", "argparser")
 
 library(tools)
 library(tidyverse)
-library(optparse)
+library(argparser)
 
-parser <- OptionParser(usage = "%prog [options] file")
+p <- arg_parser("Grade Result Average Summrizer", hide.opts = TRUE)
 
-arguments <- parse_args(parser, positional_arguments = 1)
+p <- add_argument(p, "file", help = "input file")
+p <- add_argument(
+    p,
+    "--scale",
+    help = "scaling factor for total score",
+    default = "1"
+)
 
-score_file <- normalizePath(arguments$args)
+args <- parse_args(p)
+
+score_file <- normalizePath(args$file)
+scale <- args$scale %>% as.numeric()
 
 if (file.access(score_file) == -1) {
     stop(sprintf("Specified file (%s) does not exist", file))
 } else {
     score <- read_csv(score_file) %>%
     group_by(name, perm) %>%
-    summarize(score = ceiling(sum(score_received) * 1.25))
+    summarize(score = ceiling(sum(score_received) * scale))
 
     path <- dirname(score_file)
     base <- basename(score_file)
@@ -36,7 +45,9 @@ if (file.access(score_file) == -1) {
         )
     )
 
-    (ggplot(score, aes(x = score)) + geom_histogram(bins = 5)) %>%
+    (
+        ggplot(score, aes(x = score)) + geom_histogram(bins = 5)
+    ) %>%
     ggsave(
         filename = file.path(
             path,
